ea6ca4ff679ffe87679ddfecc72d0802e6a5621f
Tommy Tran <haitran1995@gmail.com>
2023-08-14 08:15:03 -0400

    Add: post request route

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ const pool = require("./db");
 app.use(cors());
 app.use(express.json());
 
+//ROUTES//
+
+// Adding data to database
+app.post("/post", async (req, res) => {
+  try {
+    const { first_name } = req.body;
+    const newPost = await pool.query(
+      "INSERT INTO names (first_name) VALUES($1)",
+      [first_name]
+    );
+
+    res.json(newPost);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
 app.listen(5001, () => {
   console.log("server has started on port 5001");
 });
