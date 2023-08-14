6317959a1f33a9ebb3cfa0cfdec4f4b35d5dfd40
Tommy Tran <haitran1995@gmail.com>
2023-08-14 06:19:45 -0400

    Added: nodemon start script

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 
 //middle ware
 app.use(cors());
+app.use(express.json());
 
 app.listen(5001, () => {
   console.log("server has started on port 5001");
